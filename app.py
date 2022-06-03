d0c3aa727ab03fe476649c19219ac3e73917d6b2
Shruti Sureshan <shrutisureshan@gmail.com>
2022-06-03 10:40:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH ='malaria_detector.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+
+
+
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(130,130,3))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x = np.expand_dims(x, axis=0)
+
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+
+    preds = model.predict(x)
+    if preds==[[0.]]:
+        preds="Malaria Infected"
+    else:
+        preds="Uninfected"
+    
+    
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
